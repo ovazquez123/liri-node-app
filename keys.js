fef23431287ff262838483575cf3798b40ed5514
Omar Esai Vazquez <ovazquez@eng.ucsd.edu>
2017-10-14 08:19:14 -0700

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<8LP5BZ9w5Lqci68Bky8uORgDz>',
+  consumer_secret: '<RLa5LUKlplEe6XjiX11GXCXvKUrxGWvO6yU47AC4yzWbBIWZ2u>',
+  access_token_key: '<889876645414191104-WNfjwzKAdVcNq4NTN9OIjtM6jRClxiL>',
+  access_token_secret: '<UZari6yfORFlRXfwchCmsLYhVfFziQ2QlZ6DItG3NIa3q>',
+}
+
+module.exports = twitterKeys;
